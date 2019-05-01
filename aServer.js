60c1912ed303a203261286db7eb4b5ff5495bca3
Alisa Hussain <alisa.hussain@hotmail.com>
2019-05-01 20:22:18 +0100

    fixed date, invitees broken

--- aServer.js
+++ aServer.js
@@ -3,7 +3,10 @@
 * -call above function on page loading and creating event
 * -on clicking a card button, get people from event and on client side fill in the carousel
 * -search events and display relevant cards
-* -set default values for people on carousel*/
+* -set default values for people on carousel
+* -search people
+* -authentication
+ * call client function await post to server*/
 
 
 var express = require('express');
@@ -14,11 +17,12 @@ var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({extended:false});
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
-var fs = require('fs');
+const fs = require('fs');
 var eventData = require('./client/eventData.json');
 var peopleData = require('./client/peopleData.json');
 
 app.post('/newevent', urlencodedParser,  async function(req, resp){
+    console.log('in newevent')
     var anEvent = new Object();
     console.log(req.body);
     anEvent.eventName = req.body.eventName;
@@ -27,26 +31,22 @@ app.post('/newevent', urlencodedParser,  async function(req, resp){
     anEvent.aDateTime = req.body.aDateTime;
     anEvent.invitees = req.body.invitees;
     eventData.push(anEvent);
-    eventData.forEach(function(item, index, array){
-        console.log(item.eventName)
-    })
 
-    await fs.readFile('eventData.json', 'utf8',  function readFileCallback(err, data) {
+    await fs.readFile('./client/eventData.json', 'utf8',  function readFileCallback(err, data) {
         if (err){
             console.log(err);
         } else {
-            obj = JSON.parse(data); //now it an object
+            var obj = JSON.parse(data); //now it an object
             obj.push(anEvent); //add some data
-            json = JSON.stringify(obj); //convert it back to json
-            fs.writeFile('eventData.json', json, 'utf8', callback); // write it back
+            var json = JSON.stringify(obj); //convert it back to json
+            fs.writeFile('./client/eventData.json', json, 'utf8', callback); // write it back
         }
 
     });
 
     console.log('form submitted ' + req.body.eventName);
-    alert("An event has been added- click refresh to display all events.")
-    resp.status(204).send();
-})
+    resp.status(200).send('Success!');
+});
 
 function callback(err, data) {
     if (err) {
@@ -57,8 +57,8 @@ function callback(err, data) {
 }
 
 app.post('/newperson', urlencodedParser, function(req, resp){
+    //event.preventDefault()
     var aPerson = new Object();
-    console.log(req.body);
     aPerson.fullname = req.body.fullname;
     aPerson.description = req.body.description;
     aPerson.dob = req.body.dob;
@@ -66,45 +66,43 @@ app.post('/newperson', urlencodedParser, function(req, resp){
     peopleData.forEach(function(item, index, array){
         console.log(item.fullname)
     });
-    fs.readFile('peopleData.json', 'utf8', function readFileCallback(err, data) {
+    fs.readFile('./client/peopleData.json', 'utf8', function readFileCallback(err, data) {
         if (err){
-            console.log(err);
+            console.log('error in reading peopleData ',err);
         } else {
             obj = JSON.parse(data); //now it an object
             obj.push(aPerson); //add some data
             json = JSON.stringify(obj); //convert it back to json
-            fs.writeFile('peopleData.json', json, 'utf8', callback); // write it back
+            console.log(json);
+            fs.writeFile('./client/peopleData.json', json, 'utf8', callback); // write it back
         }
     });
 
     console.log('form submitted ' + req.body.fullname);
-    resp.send('form submitted 1 ' + aPerson);
+    //refresh invitees list in create event
+
+    resp.status(204).send('person added');
 });
 
-app.get('/refresh', function(req, resp) {
+app.get('/getEvents', function(req, resp) {
     try {
         resp.send(eventData);
 
     } catch(err) {
-        console.log('error in refresh')
+        console.log('error in server getEvents', err);
         resp.status(400).send('');
     }
 });
 
+app.get('/getPeople', function(req, resp) {
+   try {
+       resp.send(peopleData);
+   } catch(err) {
+       console.log('error in server getPeople', err);
+       resp.status(400).send('');
+   }
+});
 
-/*<a class="list-group-item list-group-item-action" href="#list-item-a">Ali'sa's Partay</a>*/
 
-/*<div class = "col-auto mb-3" id="list-item-1">
-    <div class = "card" style ="width:18rem">
-    <div class="card-header">
-    5th March 2019
-</div>
-<div class = "card-body">
-    <h5 class = "card-title">Ali'sa's Partay</h5>
-<p class="card-text">This party bangs. exclusives only.</p>
-<a href="#" class = "btn btn-primary card-button">Click here to see those invited.</a>
-</div>
-</div>
-</div>*/
 
 app.listen(8090);
--- client/aClient.js
+++ client/aClient.js
@@ -1,38 +1,241 @@
-document.getElementById("refreshEventsBtn").addEventListener('click', async function (event) {
+window.onload = async function() {
     try {
-        console.log('in refresh events');
-        let response = await fetch('http://127.0.0.1:8090/refresh');
+        updateInviteesList(false);
+        fillEventCards()
+    } catch(e) {
+        console.log('page not found', e)
+    }
+};
+
+let refButton = document.getElementById("refreshEventsBtn");
+refButton.addEventListener('click', async function (event) {
+fillEventCards()
+});
+
+let fillEventCards = async () => {
+    try {
+        let response = await fetch('http://127.0.0.1:8090/getEvents');
         //eventJSON is the JSON.parse version of the data
         let eventJSON = await response.text();
+        let eventObj = JSON.parse(eventJSON);
 
         let cardsDiv = document.getElementById('eventCardCollection');
-        let cardsSidebarDiv = document.getElementById('eventSidebar');
+        let cardsSidebarDiv = document.getElementById('eventSideBar');
         let cardsHtml, cardsSidebarHtml;
 
-        cardsHtml = eventJSON.map(function(anItem, anIndex) {
+        cardsHtml = eventObj.map(function(anItem, anIndex) {
+            let formattedDateTime = formatDateTime(anItem.aDateTime);
             return ('<div class = "col-auto mb-3" id="list-item-'+ anIndex + '">' +
                 '<div class = "card" style ="width:18rem">' +
                 '<div class="card-header">' +
-                anItem.aDateTime +
+                formattedDateTime +
                 '</div>' +
                 '<div class = "card-body">' +
-                '<h5 class = "card-title">' + anItem.eventName + '</h5>' +
+                '<h5 id = "title"'+anIndex+' class = "card-title">' + anItem.eventName + '</h5>' +
                 '<p class="card-text">' + anItem.description+'</p>' +
-                '<a href="#" class = "btn btn-primary card-button"> Click here to see those invited.</a>' +
+                '<button id="btn'+anIndex+'"class = "inviteeBtn btn btn-primary card-button "> Click here to see those invited.</button>' +
                 '</div>' +
                 '</div>' +
                 '</div>')
         }).join('');
 
-        cardsSidebarHtml = eventJSON.map(function(anItem, anIndex) {
+
+        cardsSidebarHtml = eventObj.map(function(anItem, anIndex) {
             return('<a class="list-group-item list-group-item-action" href="#list-item-'+ anIndex+ '">'+ anItem.eventName  + '</a>')
         }).join('');
         cardsSidebarDiv.innerHTML = cardsSidebarHtml;
         cardsDiv.innerHTML = cardsHtml;
 
+        for (let eventIndex = 0; eventIndex < eventObj.length; eventIndex++) {
+            addCardEventListener(eventObj, eventIndex);
+        }
+
+
     } catch(e) {
-        console.log('page not found');
+        console.log(e);
+    }
+
+
+};
+
+const addCardEventListener = (displayedEvents, cardIndex) => {
+  let buttonId = 'btn'+cardIndex;
+  document.getElementById(buttonId).addEventListener('click', () => {
+      //displayedEvents holds all events currently displayed
+      displayPeopleCarousel(displayedEvents, cardIndex)
+          .catch(function (error) {
+              console.log(error);
+
+          })
+  })
+};
+
+async function displayPeopleCarousel(displayedEvents, i) {
+    try {
+        //i is the index value of the card
+        //titleIndex is the id of the title
+        let titleIndex= 'title'+i;
+        var eventTitle = document.getElementById(titleIndex);
+        //eventObj is an object with all existing events
+        let eventResponse = await fetch('http://127.0.0.1:8090/getEvents');
+        let eventJSON = await eventResponse.text();
+        let eventObj = JSON.parse(eventJSON);
+
+        //inviteeNameArray holds all invitees of the clicked event
+        let inviteeNameArray = [];
+        if ((eventObj[i].invitees).length === 1) {
+            inviteeNameArray.push(eventObj[i].invitees[0]);
+
+        } else {
+            inviteeNameArray = eventObj[i].invitees;
+        }
+
+        //peopleObj holds all existing people
+        let peopleResponse = await fetch('http://127.0.0.1:8090/getPeople');
+        let peopleJSON = await peopleResponse.text();
+        let peopleObj = JSON.parse(peopleJSON);
+
+        //peopleDisplayObj is to hold people objects of the invitees of the clicked invitees
+        var peopleDisplayObj = [];
+
+        //Go through each invitee and compare to each existing person until match is found
+        //inviteeIndex is the index of invitees to the clicked event
+        for (var inviteeIndex = 0; inviteeIndex < inviteeNameArray.length; inviteeIndex++) {
+            var found = false;
+            var peopleIndex = 0;
+            //j is the index of all existing people
+            while (found === false && peopleIndex < peopleObj.length) {
+                if (inviteeNameArray[inviteeIndex] === peopleObj[peopleIndex].fullname
+                ) {
+                    peopleDisplayObj.push(peopleObj[peopleIndex]);
+                    found = true
+                } else {
+                    peopleIndex = peopleIndex + 1;
+                }
+            }
+            if (found === false) {
+                console.log('error- person not found');
+            }
+        }
+        let carouselItemsDiv = document.getElementById('carouselItems');
+        let carouselSliderDiv = document.getElementById('carouselSlider');
+
+        let carouselHtml = peopleDisplayObj.map(function(anItem, anIndex) {
+            if (anIndex === 0) {
+                var activeVal = 'active'
+            } else {
+                activeVal = ''
+            }
+            //calculate age of invitee from date of birth
+            let age = calcAge(peopleDisplayObj[anIndex].dob);
+
+            return ('<div class="carousel-item '+ activeVal+'">' +
+                '<div class="inner">'+
+                '<h2>'+peopleDisplayObj[anIndex].fullname+'</h2>'+
+                '<h3>Age: '+ age+ '</h3>'+
+                '<h3>'+ peopleDisplayObj[anIndex].description +'</h3>'+
+                '</div>'+
+                '</div>')
+        }).join('');
+
+        let carouselSliderHtml = peopleDisplayObj.map(function(anItem, anIndex) {
+            if (anIndex === 0) {
+                return ('<li data-target="#controls" data-slide-to="0" class="active"></li>')
+            } else {
+                return('<li data-target="#controls" data-slide-to="'+anIndex+'"></li>')
+            }
+        }).join('');
+
+        carouselItemsDiv.innerHTML = carouselHtml;
+        carouselSliderDiv.innerHTML = carouselSliderHtml
+
+    } catch(err) {
+        console.log('error in displayPeopleCarousel: ', err);
+    }
+
+
+
+
+};
+
+const calcAge = (dob) => {
+    let unformattedDob = new Date(dob);
+    let today = new Date();
+    var yearsDiff = (today.getTime()-unformattedDob.getTime())/1000;
+    yearsDiff /= (60*60*24);
+    return Math.abs(Math.round(yearsDiff/365.25));
+
+};
+
+const formatDateTime = (aDateTime) => {
+    var aDateTime = aDateTime.split('T');
+    var date = aDateTime[0].split('-');
+    var time = aDateTime[1].split(':');
+    return new Date(date[0], date[1], date[2], time[0], time[1], time[2], 0);
+};
+
+const updateInviteesList = async (isNew) => {
+    let response = await fetch('http://127.0.0.1:8090/getPeople');
+    let peopleJSON = await response.text();
+    let peopleObj = JSON.parse(peopleJSON);
+    if (isNew === true) {
+        var newName = document.getElementById('fullName').value;
+        var tempObj = {fullname: newName};
+        peopleObj.push(tempObj);
+    }
+    let inviteesDiv = document.getElementById('someInvitees');
+    let inviteesHtml= peopleObj.map(function(anItem, anIndex) {
+        return ('<option>'+anItem.fullname+'</option>')}).join('');
+    inviteesDiv.innerHTML = inviteesHtml;
+    if (isNew) {
+        alert('Your new person has been created.');
+    }
+
+};
+
+document.getElementById('addEvent').addEventListener('submit', async function(event) {
+    event.preventDefault();
+    console.log('in the new addEvent aclient.js');
+    try {
+        let eventName = document.getElementById('anEventName').value;
+        let description = document.getElementById('aDescription').value;
+        let location = document.getElementById('aLocation').value;
+        let aDateTime = document.getElementById('aDateTime').value;
+        if (aDateTime.length === 16) {
+            aDateTime = aDateTime + ':00';
+        }
+        let invitees = document.getElementById('someInvitees').value;
+        console.log('invitees ', invitees);
+        let response = await fetch('http://127.0.0.1:8090/newevent',
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded"
+                },
+                body: "eventName="+ eventName+
+                    "&description="+ description+
+                    "&location="+  location+
+                    "&aDateTime="+aDateTime+
+                    "&invitees="+invitees
+                });
+        if (!response.ok) {
+            throw new Error("problem adding an event "+response.code )
+            }
+        else {
+            alert(`Your event ${eventName}, was added.`)
+        }
+    }   catch(error) {
+        console.log('error: '+ error);
+    }
+    fillEventCards()
+        .then(function(value) {
+            console.log('Successfully filled cards');
+        })
+        .catch = (error) => {
+        console.log(error);
     }
 
 
 });
+
+
